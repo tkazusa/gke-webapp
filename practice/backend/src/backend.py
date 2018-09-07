596a9b7dc7f4687e3b7f2a124c8ef8330e69eeb3
tkazusa <tkazusa@gmail.com>
2018-09-07 14:52:06 +0000

    add practice

---
+++ practice/backend/src/backend.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from flask import Flask, jsonify, request
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def get():
+    name = "hello get"
+    return name
+
+@app.route('/', methods=['POST'])
+def post():
+    changed_name = request.json['name'] + str(request.json['age'])
+    response = jsonify(changed_name)
+    response.status_code = 200
+    return response
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
