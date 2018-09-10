02cc9c5fc4ba0dafae9d1cd151efd06ccd7ed68c
Taketoshi Kazusa <takekazusa@gmail.com>
2018-09-10 05:02:17 +0000

    mod

--- frontend/src/frontend.py
+++ frontend/src/frontend.py
@@ -23,18 +23,19 @@ def render_input_form():
 @app.route('/', methods=["POST"])
 def result():
     INPUT_DATA = {
-        "Pclass": [request.form["pclass"]],
-        "Name": [request.form["name"]],
-        "Ticket": [request.form["ticket"]],
-        "Sex": [request.form["sex"]],
-        "Age": [request.form["age"]],
-        "SibSp": [request.form["Sibsp"]],
-        "Parch": [request.form["parch"]],
-        "Cabin": [request.form["cabin"]],
-        "Embarked": [request.form["embarked"]]}
+        "Pclass": request.form["pclass"],
+        "Name": request.form["name"],
+        "Ticket": request.form["ticket"],
+        "Sex": request.form["sex"],
+        "Age": request.form["age"],
+        "SibSp": request.form["Sibsp"],
+        "Parch": request.form["parch"],
+        "Cabin": request.form["cabin"],
+        "Embarked": request.form["embarked"]}
 
-    response = requests.post(URL, data=json.dump(INPUT_DATA), headers=headers)
+    response = requests.post(URL, data=json.dumps(INPUT_DATA), headers=headers)
     return render_template('result.html', status=response.text)
+
 
 if __name__ == "__main__":
     app.run(debug=False, host="0.0.0.0", port=8080)
